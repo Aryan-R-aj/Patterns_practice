fb3ddf3e020701fb881821d3499820f92b7e0e4d
Aryan-R-aj <your.aryanrajssm007.com>
2025-01-21 10:43:02 +0530

    Second Pattern

---
+++ pattern_no2.java
@@ -0,0 +1,18 @@
+public class pattern_no2 {
+    public static void main(String[] args) {
+        for(int row=1; row<=5; row++){
+            for(int col =1; col <= 5; col++){
+                System.out.print("*" + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
+/*
+pattern output
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+               */
