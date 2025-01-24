8d0a0feafde37264a49c351869cbfca37ad7e52c
Aryan-R-aj <your.aryanrajssm007.com>
2025-01-24 12:06:05 +0530

    add another pattern

---
+++ Pattern_no4.java
@@ -0,0 +1,10 @@
+public class Pattern_no4 {
+    public static void main(String arg[]){
+        for(int row =1; row <= 5; row++){
+            for(int col =1; col <= row; col++){
+                System.out.print(col + "");
+            }
+            System.out.println("");
+        }
+    }
+}
